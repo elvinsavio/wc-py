b06660da7681adef1f119750c87dacf75e6c7623
elvinsavio <elvinsavio.lp@gmail.com>
2025-12-04 21:29:51 +0530

    handle multiple files

--- src/cli.py
+++ src/cli.py
@@ -1,31 +1,42 @@
 import click
-from typing import Optional
 from .file_handler import FileHandler, MultiFileHandler
+import os
 
+
+def _is_dir(filenames: tuple[str]):
+    for filename in filenames:
+          if os.path.isdir(filename):
+               return True
+    return False
+
+def wc_files(filenames: tuple[str], len_files: int, collection: bool):
+    if len_files == 1:
+        f = FileHandler(filename=filenames[0])
+        f.print_count()
+        if collection:
+            f.print_collection()
+    else:
+         f = MultiFileHandler(filenames=filenames)
+         f.print_count()
+         if collection:
+              f.print_collection()
 
 @click.command()
 @click.argument("filenames", nargs=-1)
 @click.option(
     "-c", "--collection", is_flag=True, help="Collective count of all words"
 )
-@click.option(
-    "-r", "--recursive", is_flag=True, help="Recursively go through several files"
-)
-def wc(filenames: tuple[str], recursive: Optional[bool] = False, collection: Optional[bool] = False):
+def wc(filenames: tuple[str], collection: bool = False):
     """gets word count of a file"""
+    len_files = len(filenames)
 
     if not filenames:
-            click.echo("No files provided.")
-            return
+            raise ValueError("Error: No files provided.")
+            
+    is_dir = _is_dir(filenames=filenames)
 
-    if len(filenames) == 1:
-        f = FileHandler(filename=filenames[0])
-        f.print_count()
-        if collection:
-            f.print_collection()
+    if len_files > 1 and is_dir:
+         raise ValueError(f"Error: Only 1 directory can be scanned at a time \nExpected 1 got {len_files}.")
 
-    else:
-         f = MultiFileHandler(filenames=filenames)
-         f.print_count()
-         if collection:
-              f.print_collection()
+    if not is_dir:
+        wc_files(filenames=filenames, len_files=len_files, collection=collection)
